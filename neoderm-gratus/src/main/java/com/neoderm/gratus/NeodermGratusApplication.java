0182c5d8aa4cc48ca9b219064c0237d6cd041519
lilujun <lilujun1993@icloud.com>
2022-10-12 09:43:35 +0800

    加入admin控制台

--- neoderm-gratus/src/main/java/com/neoderm/gratus/NeodermGratusApplication.java
+++ neoderm-gratus/src/main/java/com/neoderm/gratus/NeodermGratusApplication.java
@@ -1,8 +1,12 @@
 package com.neoderm.gratus;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
+@EnableKnife4j
 @SpringBootApplication
 public class NeodermGratusApplication {
 
---
+++ neoderm-visual/admin-server/src/main/java/com/neoderm/monitor/AdminServerApplication.java
@@ -0,0 +1,18 @@
+package com.neoderm.monitor;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @date 2018年06月21日 监控中心
+ */
+@EnableAdminServer
+@SpringBootApplication
+public class AdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+
+}
